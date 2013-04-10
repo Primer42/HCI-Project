99382a3e9ea2be9d4fba72087c2a87f6f3c6bc1f
William Richard <willster3021@gmail.com>
2013-04-09 21:35:02 -0400

    Made the top menu bar look better.

--- higher-fidelity-prototype/lib/template.php
+++ higher-fidelity-prototype/lib/template.php
@@ -12,20 +12,20 @@
 <body>
 
 <div id="header">
-<table>
-<tr>
-<td><h3><img src="./img/jobtracker.png"> <br>JOB TRACKER 5000</h3> </td>
-<td><a href="./company.php"> <img src="./img/company.png"> <br>Companies</a></td>
-<td><a href="./job.php"> <img src="./img/jobs.png"> <br>Jobs</a></td>
-<td><a href="./person.php"> <img src="./img/people.png"> <br>People</a></td>
-<td><a href="./note.php"> <img src="./img/notes.png"> <br>Notes</a></td>
-</tr>
-</table>
+
+<ul>
+<li><a href="./start.php"> <img src="./img/jobtracker.png"><br>JOB TRACKER</b></a></li>
+<li><a href="./company.php"> <img src="./img/company.png"><br>Companies</a></li>
+<li><a href="./job.php"> <img src="./img/jobs.png"> <br>Jobs</a></li>
+<li><a href="./person.php"> <img src="./img/people.png"><br>People</a></li>
+<li><a href="./note.php"> <img src="./img/notes.png"><br>Notes</a></li>
+</ul>
+
 </div>
 
-<br>
-
+<div>
 <?php echo getPageContent();?>
+</div>
 
 
 </body>
